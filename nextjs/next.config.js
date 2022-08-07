e1ef7776cb7e4a3e5c1bd824c1dc24b9dad3dca0
akasen <akasen.org@gmail.com>
2022-08-07 09:22:18 +0900

    index page now redirects to /process-estimate-sheet (temporarily)

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/process-estimate-sheet",
+        permanent: false,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
